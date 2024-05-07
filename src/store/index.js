cca0fbfb2cb93032430ca75540b77907ccbe22c8
SantyOkami23 <santiago.ramirez5@utp.edu.co>
2024-05-06 23:15:09 -0500

    Se obtienen personajes modificando el index.js y creando los states, mutations y URL

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,31 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    characters: [],
+    charactersFilter: []
+    
   },
   getters: {
+    setCharacters(state, payload ){
+      state.characters = payload
+    },
+
+    setCharactersFilter(state, payload){
+      state.charactersFilter = payload
+    }
   },
   mutations: {
+    async getCharacters({commit}){
+      try{
+        const response = await fetch('https://rickandmortyapi.com/api/character')
+        const data = await response.json()
+      console.log(data)
+
+      }catch(error){
+      console.error(error)
+      }
+    }
+
   },
   actions: {
   },
