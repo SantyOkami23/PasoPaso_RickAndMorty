f4144019538b81f6c5fd53ee500c7a1adf349a51
SantyOkami23 <santiago.ramirez5@utp.edu.co>
2024-05-08 13:28:14 -0500

    Se crea componente Card

--- src/components/ListCharacters.vue
+++ src/components/ListCharacters.vue
@@ -2,7 +2,7 @@
     <section>
         <div class="characters">
             <div class="characters__item" v-for="character in characters" :key = "character.id">
-               {{ character.name }}
+                <CardCharacter :character="character"/>
             </div>
         </div>
     </section>
@@ -13,7 +13,13 @@
 
 import {onMounted, computed} from 'vue'	
 import {useStore} from 'vuex'	
+import CardCharacter from '@/components/CardCharacter.vue'
+
 export default {
+    components:{
+        CardCharacter
+    },
+
     setup(){
 
         const store = useStore()
